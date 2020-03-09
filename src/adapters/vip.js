d637506f960171bd00842c3e5c0702317593b880
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2020-03-09 22:30:35 +0100

    Temporary escape bad character & because one url currently breaks the xml (Line Number 4016, Column 77 in the vip xml)

--- src/adapters/vip.js
+++ src/adapters/vip.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { parseStringPromise } from "xml2js";
+function escapeBadChars(str) {
+  return str.replace(/&/g, "&amp;");
+}
 export function getSongs() {
   return new Promise((resolve, reject) => {
     axios.get("http://vip.aersia.net/roster.xml").then(res => {
-      parseStringPromise(res.data, { explicitArray: false }).then(res => {
+      parseStringPromise(escapeBadChars(res.data), {
+        explicitArray: false
+      }).then(res => {
         const tracks = res.playlist.trackList.track;
         const newTracks = tracks.map((track, index) => ({
           artist: track.creator,
